2ee3487bcd0196ab609fae323ab274ff9aa2756b
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-05 21:16:20 -0400

    Edit Rotator.cs to allow rotation axes to be individually disabled from the inspector

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,13 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    int x = 15;
+    int y = 30;
+    int z = 45;
+    public Boolean disableX = false;
+    public Boolean disableY = false;
+    public Boolean disableZ = false;
 
-    // Update is called once per frame
     void Update()
-    {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    {   
+        if (disableX)
+        {
+            x = 0;
+        }
+        else
+        {
+            x = 15;
+        }
+        if (disableY)
+        {
+            y = 0;
+        }
+        else
+        {
+            y = 30;
+        }
+        if (disableZ)
+        {
+            z = 0;
+        }
+        else
+        {
+            z = 45;
+        }
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
     }
 }
